f11ac77912d35c9e3a4f37f9f019f3970dd80195
Mufeng Tang （唐 沐丰） <tangmufeng424@gmail.com>
2021-02-23 00:11:46 -0600

    Add files via upload

---
+++ gentrials_200718.m
@@ -0,0 +1,104 @@
+%% Basic set-up
+close all;
+clearvars;
+PsychDefaultSetup(2);
+Screen('Preference', 'SkipSyncTests', 1); 
+% ignore warning msg output; won't be a problem when presented on experimental monitor
+Screen('Preference','VisualDebugLevel', 0);
+screens = Screen('Screens');
+% automatically displays to highest numbered screen, e.g. external screen
+% if there is one present
+screenNumber = max(screens);
+white = [255,255,255];sca
+
+black = [0,0,0];
+% recalculate mean screen luminance of white dots on black for grey (eventually)
+grey = white/2;
+
+%% This script creates ntotaltrials split into .mov files of ten trials each
+
+ntotaltrials = 10;
+ntrials = 10;
+nfiles = ntotaltrials/ntrials;
+% save video file in dimensions dim(width,height)
+% except this is not working and the video file is exactly the size of the
+% monitor... 
+% dim = [36 64];
+% set up parameters
+coh = 1;
+incoh = .15;
+dirs = [0, 90, 180, 270];
+dsize = 9; % pixels
+dspeed = 45;
+ndots = 150; % originally 460
+catch_dur = 4;
+n_catch_trials = round(ntrials/2);
+n_change_trials = ntrials - n_catch_trials;
+% change will take place at a variable time (500 - 3500 ms)
+change_time_min = 0.5;
+change_time_max = 3.5;
+change_time_detect = 0.6;
+grey_dur = 4;
+
+% set up dots
+dots.nDots = ndots;
+dots.color = white;
+dots.speed = dspeed/3;
+dots.size = dsize;
+dots.coherence = coh;
+dots.direction = dirs(1);
+dots.center = [0,0];
+dots.apertureSize = [42, 42];
+
+%% Begin loop to create nfiles with ntrials each
+
+for fileidx = 1:nfiles
+    % you may want to split this up; depending on your set-up you may not be able to use your screen as the script runs
+    
+    % set up trial sequence for this mov file
+    seq = zeros(1,ntrials);
+    seq(randperm(ntrials,n_catch_trials)) = 1; % 1 means change, 0 means no change
+    trialdir = zeros(1,ntrials);
+    trialcoh = zeros(1,ntrials);
+    trialcoh(randperm(ntrials,n_catch_trials)) = 1; % 1 means start at coh, 0 means incoh
+    changetimes = zeros(1,ntrials);
+    trialend = zeros(1,ntrials);
+    for ii = 1:length(trialdir) % 1:ntrials
+        diridx = randi([1,4]);
+        trialdir(ii) = dirs(diridx); % one of the four directions
+        if seq(ii)~=0 % seq=1 means change, 0 means no change
+            % Randomly choose when the change occurs during change trials
+            changetimes(ii) = randi([change_time_min*1000, change_time_max*1000])/1000;
+            % Trial ends 0.6s after the change occurs and next trial begins
+            % trialend(ii) = changetimes(ii) + change_time_detect; 
+            % to keep each trial at the same length (frames), remove this for now
+            trialend(ii) = catch_dur;
+        else
+            trialend(ii) = catch_dur;
+        end
+    end
+
+    % calculate total experiment duration
+    duration = sum(trialend);
+
+    %% Make stim!
+    % open window and color the background black
+    % changed the "rect" parameter here to open only a small (non-full screen) window
+    [window, windowRect] = PsychImaging('OpenWindow', screenNumber, black, [0 0 320 180]);
+    display.frameRate = FrameRate(window); % ~60frames/s, will be rounded to 60 in createDriftingDots.m
+    % not necessary: dots.lifetime = display.frameRate*duration;
+    display.resolution = zeros(1,2);
+    [display.resolution(1), display.resolution(2)] = Screen('WindowSize',window);
+    display.width = 30; % of monitor itself; cm (can change)
+    display.dist = 50; % of monitor itself; cm (can change)
+    saveVideo = true;
+    createDriftingDots(coh,incoh,window,display,dots,grey,grey_dur,seq,trialdir,trialcoh,trialend,changetimes,saveVideo,fileidx);
+
+% Saves the movie file and its corresponding info, i.e. trial sequence,
+% trial type, coherence change time (if any)
+
+%% Other considerations
+% May eventually want to temporally extrapolate s.t. each "frame" is 1ms.
+
+end
+Screen('CloseAll');
--- tfrecord_dataset.py
+++ tfrecord_dataset.py
@@ -1,11 +1,18 @@
+"""
+Load .tfrecord movies data and metadata
+"""
+
 import os
 import io
-import blosc
+# import blosc
 import numpy as np
 import tensorflow as tf
 import keras
+import matplotlib.pyplot as plt
+import argparse
 
 HOME_PATH = '/home/macleanlab/mufeng/NaturalMotionCNN/models/'
+IMG_PATH = '/home/macleanlab/mufeng/tfrecord_data_processing/plots/'
 PI = np.pi
 
 def pool_fn(_a):
@@ -15,15 +22,15 @@ def pool_fn(_a):
 
 
 def create_dataset(paths, max_seq_len=4800, encoding='png', pool=None):
-    # again, you will change the features to reflect the variables in your own metadata
-    # you may also change the max_seq_len (which is the maximum duration for each trial in ms)
+    # for coherence change task: coherence_label and is_changes are variables of interest
     feature_description = {
         'frames': tf.io.VarLenFeature(tf.string),
         'change_label': tf.io.VarLenFeature(tf.int64),
         'coherence_label': tf.io.VarLenFeature(tf.int64),
         'direction_label': tf.io.VarLenFeature(tf.int64),
         'dominant_direction': tf.io.VarLenFeature(tf.int64),
-        'trial_coherence': tf.io.VarLenFeature(tf.int64)
+        'trial_coherence': tf.io.VarLenFeature(tf.int64),
+        'is_changes': tf.io.VarLenFeature(tf.int64),
     }
     data_set = tf.data.TFRecordDataset(paths)
 
@@ -41,110 +48,71 @@ def create_dataset(paths, max_seq_len=4800, encoding='png', pool=None):
             # frames: the encoded video
             _frames = tf.py_function(func=fn, inp=[_x['frames'].values[0]], Tout=tf.uint8)[:max_seq_len]
 
-        # This whole padding section is not needed, as we have movies and trials of fixed size
-        """
-        _m1 = tf.cast(tf.random.uniform(minval=0, maxval=1, shape=()) > .5, tf.int32) * 2 - 1
-        _m2 = tf.cast(tf.random.uniform(minval=0, maxval=1, shape=()) > .5, tf.int32) * 2 - 1
-        _frames = _frames[:, ::_m1, ::_m2]
-        _seq_len = tf.shape(_frames)[0]
-        _p1 = [0, max_seq_len - _seq_len]
-        _p = [_p1, [0, 0], [0, 0], [0, 0]]
-        _frames = tf.pad(_frames, _p)
-
-        _label = tf.pad(_x['coherence_label'].values[:max_seq_len], [_p1])
-        _change_label = tf.pad(_x['change_label'].values[:max_seq_len], [_p1])
-        _change_label += tf.pad(_change_label[:-23], [[23, 0]])
-        _direction_label = tf.pad(_x['direction_label'].values[:max_seq_len], [_p1])
-        _dominant_direction = _x['dominant_direction'].values[:max_seq_len]
-        _trial_coherence = _x['trial_coherence'].values[:max_seq_len]
-        """
-
-        _label = _x['coherence_label'].values
+        _coh_label = _x['coherence_label'].values
         _change_label = _x['change_label'].values
         _direction_label = _x['direction_label'].values
         _dominant_direction = _x['dominant_direction'].values
         _trial_coherence = _x['trial_coherence'].values
+        _is_changes = _x['is_changes'].values
 
-        return _frames, dict(tf_op_layer_coherence=_label, # 0 or 1, length 4800
+        return _frames, dict(tf_op_layer_coherence=_coh_label, # 0 or 1, length 4800
                              tf_op_layer_change=_change_label, # 0 or 1, length 4800
                              tf_op_dir=_direction_label, # 1,2,3,4, length 4800
                              tf_dom_dir=_dominant_direction, # 1,2,3,4, length 10 (i.e. per trial/grey screen)
                              tf_trial_coh=_trial_coherence, # 0 or 1, length 10
-                             )
+                             tf_is_changes=_is_changes,
+                            )
 
     data_set = data_set.map(_parse_example, num_parallel_calls=24)
     return data_set
 
-
-# angs an (n,1) array
-def angs_to_categories(angs):
-    categories = np.zeros_like(angs)
-    for i in range(angs.shape[0]):
-        ang = angs[i,0]
-        if ang >= -PI/4 and ang < PI/4:
-            categories[i,0] = 1
-        elif ang >= PI/4 and ang < 3*PI/4:
-            categories[i,0] = 2
-        elif (ang >= 3*PI/4 and ang <= PI) or (ang >= -PI and ang < -3*PI/4):
-            categories[i,0] = 3
-        elif ang >= -3*PI/4 and ang < -PI/4:
-            categories[i,0] = 4
-    return categories
-
-
 def main():
-    file_names = [os.path.expanduser(f'preprocessed/processed_data_{i+1}.tfrecord') for i in range(30)]
+    parser = argparse.ArgumentParser(description='Args for reading in tfrecord files')
+    parser.add_argument('num_files', type=int, help='number of tfrecord files to read in')
+    args = parser.parse_args()
+
+    # for now, each tfrecord file corresponds to only one movie
+    file_names = [os.path.expanduser(f'preprocessed/processed_data_{i+1}.tfrecord') for i in range(args.num_files)]
     data_set = create_dataset(file_names, 4800).batch(1)
 
     # ex[0] the frames (tensor), shape [1, 4800, 36, 64, 3]
     # ex[1] a dictionary, containing coh level, change label and direction
     k = 1
-    """
-    we want each movie to be represented by a list of trials, each trial to be represented by a 
-    4d tensor of shape (batch,240,36,64,3); for each trial in this list, pass it through the CNN.
-    """
     movies = []
-    true_dirs = []
-    trial_cohs = []
-    for ex in data_set:
+    trial_cohs = [] # original coherence level for each trial
+    coh_labels = [] # coherence level at each frame
+    is_changes = [] # whether coherence changes for each trial
+    for ex in data_set: # iterate through the 30 movies
         trials = []
         print("Movie ", k)
-        # the direction vector, of length max_seq_len, fixed for each trial/gray screen in the movie
-        dirs = ex[1]['tf_op_dir'].numpy()[0]
-        dom_dir = ex[1]['tf_dom_dir'].numpy()[0] # len = 10, direction vector of this movie
+
         trial_coh = ex[1]['tf_trial_coh'].numpy()[0] # len = 10, coh vector of this movie
-        true_dirs.append(dom_dir)
         trial_cohs.append(trial_coh)
+        coh_label = ex[1]['tf_op_layer_coherence'].numpy()[0]
+        coh_labels.append(coh_label)
+        is_change = ex[1]['tf_is_changes'].numpy()[0]
+        is_changes.append(is_change)
+
+        # start and end frames of each trial movie (no gray screens)
         start_frames = np.arange(0, 80, 8)
         end_frames = np.arange(4, 84, 8)
         framerate = 60
-        for i in range(len(dom_dir)): # 10
+        for i in range(len(trial_coh)): # 10
             trial = ex[0][:, start_frames[i]*framerate:end_frames[i]*framerate] # [1, 240, 36, 64, 3]
             trials.append(trial)
 
-        # concatenate the trials into a large tensor
         movie = tf.concat(trials, axis=0) # [10, 240, 36, 64, 3]
         movies.append(movie)
         k+=1
     
     all_movies = tf.concat(movies, axis=0) # [300,240,36,64,3]
-    print(all_movies.shape)
-    # load the CNN model and predict the x and y coordinates
-    model_path = HOME_PATH + 'xy_model8'
-    cnn_model = keras.models.load_model(model_path)
-    # convert xy coors to angles 
-    xy_pred = cnn_model.predict(all_movies)
-    
-    coherences = np.concatenate(trial_cohs)
-    # indices of coherence levels 100%
-    coh_ind_100 = np.where(coherences==1)[0]
-    coh_ind_15 = np.where(coherences==0)[0]
-    # accuracies at different coherence levels
-    accuracy100 = (pred_directions[coh_ind_100] == true_directions[coh_ind_100]).mean()
-    accuracy15 = (pred_directions[coh_ind_15] == true_directions[coh_ind_15]).mean()
-    print(accuracy100, accuracy15)
-        
-        
+    all_trial_cohs = np.stack(trial_cohs)
+    all_coh_labels = np.stack(coh_labels)
+    all_is_changes = np.stack(is_changes)
+    print(all_is_changes)
+    print(all_trial_cohs)
+    print(all_coh_labels.shape)
+
 if __name__ == '__main__':
     main()
 
